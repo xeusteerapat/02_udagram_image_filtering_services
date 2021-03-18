998b01a6156966bf67a0edaf5407fd3e19380c96
xeusteerapat <xeus085@gmail.com>
2021-03-18 23:40:53 +0700

    [CHORE]

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,14 +8,15 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
+
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async resolve => {
     const photo = await Jimp.read(inputURL);
     const outpath =
       '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
     await photo
-      .resize(256, 256) // resize
-      .quality(60) // set JPEG quality
+      .resize(512, 512) // resize
+      .quality(75) // set JPEG quality
       .greyscale() // set greyscale
       .write(__dirname + outpath, img => {
         resolve(__dirname + outpath);
